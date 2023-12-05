eaa57c4efd469603c19483695c69efcd7e935405
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-12-05 03:06:38 +0100

    feature: add pagination and search functionality

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,5 @@
-import { lusitana } from '@/app/ui/fonts';
+// import { luistina } from '@/app/ui/fonts';
+// ${luistina.className}
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -11,9 +12,7 @@ export default function LoginForm() {
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
+        <h1 className={` mb-3 text-2xl`}>Please log in to continue.</h1>
         <div className="w-full">
           <div>
             <label
